edac250647dad4dd1576f6b20c05fbccbe1c1c95
test <test@Becks-MacBook-Pro.local>
2021-11-17 15:04:11 -0600

    adding code

--- src/components/list-item/list-item.component.jsx
+++ src/components/list-item/list-item.component.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 import "./list-item.styles.scss";
 
-const ListItem = ({ id, title, image }) => {
+const ListItem = ({ id, title, image, description, phone, email }) => {
   const openDetail = (id) => {
     //window.location = `lists/${id}`;
     console.log("TODO: detail pageruu oroh");
   };
 
   return (
-    <div className="" onClick={(id) => openDetail(id)}>
-      <img src={image} width={100} className="row" />
-      <div className="content" className="row">
-        <h1 className="title">{title}</h1>
-        <a href="/abc"></a>
-      </div>
-    </div>
+    <>
+      <td>
+        {/* title and content column */}
+        <div className="content" className="row">
+          {/* <a href="/abc"></a> */}
+          <h1 className="title">{title}</h1>
+          {description}
+        </div>
+      </td>
+
+      {/* image column */}
+      <td>
+        <img src={image} width={100} className="row" />
+      </td>
+      <td>
+        <ul>
+          <li>{phone}</li>
+          <li>{email}</li>
+        </ul>
+      </td>
+    </>
   );
 };
 
