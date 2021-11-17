0c55cc3be63f5e307136db9d551ae8661388f352
test <test@Becks-MacBook-Pro.local>
2021-11-17 15:04:37 -0600

    adding code

--- src/redux/list/list.data.js
+++ src/redux/list/list.data.js
@@ -1,28 +1,50 @@
+import { getDefaultNormalizer } from "@testing-library/react";
+
 const LIST_DATA = [
   {
     id: 1,
     title: "Bair rent d",
+    description:
+      "Bair rent d Bair rent dBair rent dBair rent dBair rent dBair rent dBair rent dBair rent dBair rent dBair rent d",
     image: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
+    phone: "7732304447",
+    email: "beck.daansuren@gmail.com",
   },
   {
     id: 2,
     title: "ajil avna",
+    description:
+      "Bair rent d Bair rent dBair rent dBair rent dBair rent dBair rent dBair rent dBair rent dBair rent dBair rent d",
     image: "https://i.ibb.co/ypkgK0X/blue-beanie.png",
+    phone: "7732304447",
+    email: "beck.daansuren@gmail.com",
   },
   {
     id: 3,
     title: "Jackets zrana",
+    description:
+      "Bair rent d Bair rent dBair rent dBair rent dBair rent dBair rent dBair rent dBair rent dBair rent dBair rent d",
     image: "https://i.ibb.co/M6hHc3F/brown-trench.png",
+    phone: "7732304447",
+    email: "beck.daansuren@gmail.com",
   },
   {
     id: 4,
     title: "asdjlga sd",
+    description:
+      "Bair rent d Bair rent dBair rent dBair rent dBair rent dBair rent dBair rent dBair rent dBair rent dBair rent d",
     image: "https://i.ibb.co/7CQVJNm/blue-tank.png",
+    phone: "7732304447",
+    email: "beck.daansuren@gmail.com",
   },
   {
     id: 5,
     title: "asdmads",
+    description:
+      "Bair rent d Bair rent dBair rent dBair rent dBair rent dBair rent dBair rent dBair rent dBair rent dBair rent d",
     image: "https://i.ibb.co/xJS0T3Y/camo-vest.png",
+    phone: "7732304447",
+    email: "beck.daansuren@gmail.com",
   },
 ];
 
